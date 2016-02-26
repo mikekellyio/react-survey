68ff5c78b865b09735c425ff6e0044af080d1bf7
Mike Kelly <mike@kellyfam.net>
2016-02-25 23:38:40 -0500

    refactor to be clearer

--- app/index.js
+++ app/index.js
@@ -1,16 +1,7 @@
+import './styles/main.scss';
+
 import React from 'react';
 import ReactDOM from 'react-dom';
-import Hello from './components/hello.jsx';
+import Hello from './components/hello';
 
-var rootInstance = null;
-
-rootInstance = ReactDOM.render(<Hello />, document.getElementById('app'));
-
-if (module.hot) {
-  require('react-hot-loader/Injection').RootInstanceProvider.injectProvider({
-    getRootInstances: function () {
-      // Help React Hot Loader figure out the root component instances on the page:
-      return [rootInstance];
-    }
-  });
-}
+var rootInstance = ReactDOM.render(<Hello />, document.getElementById('app'));
--- webpack.config.js
+++ webpack.config.js
@@ -1,29 +1,105 @@
-var path = require('path');
+const path = require('path');
+const merge = require('webpack-merge');
+const webpack = require('webpack');
+const NpmInstallPlugin = require('npm-install-webpack-plugin');
 
-config = {
-  entry: [
-      'webpack/hot/dev-server',
-      'webpack-dev-server/client?http://localhost:8080',
-      path.resolve(__dirname, 'app/index.js')
-    ],
-    output: {
-        path: path.resolve(__dirname, 'build'),
-        filename: 'bundle.js',
-    },
-    module: {
-        loaders: [{
-            test: /\.jsx?$/,
-            loaders: ['react-hot', 'babel'],
-            include: path.join(__dirname, 'app')
-        }, {
-          test: /\.css$/, // Only .css files
-          loader: 'style!css' // Run both loaders
-        },
-        {
-          test: /\.scss$/,
-          loader: 'style!css!sass'
-        }]
-    }
+
+const TARGET = process.env.npm_lifecycle_event;
+const PATHS = {
+  app: path.join(__dirname, 'app'),
+  build: path.join(__dirname, 'build'),
+  style: path.join(__dirname, 'app/styles')
 };
 
-module.exports = config;
+process.env.BABEL_ENV = TARGET;
+
+const common = {
+  devtool: 'eval-source-map',
+  // Entry accepts a path or an object of entries. We'll be using the
+  // latter form given it's convenient with more complex configurations.
+  entry: {
+    app: PATHS.app
+  },
+  // Add resolve.extensions.
+  // '' is needed to allow imports without an extension.
+  // Note the .'s before extensions as it will fail to match without!!!
+  resolve: {
+    extensions: ['', '.js', '.jsx']
+  },
+  output: {
+    path: PATHS.build,
+    filename: 'bundle.js'
+  },
+  module: {
+    loaders: [
+      {
+        // Test expects a RegExp! Note the slashes!
+        test: /\.css$/,
+        loaders: ['style', 'css'],
+        // Include accepts either a path or an array of paths.
+        includes: [PATHS.app, 'node_modules']
+      },
+      {
+        test: /\.scss$/,
+        loaders: ['style', 'css', 'sass'],
+        include: PATHS.style
+      },
+      // Set up jsx. This accepts js too thanks to RegExp
+      {
+        test: /\.jsx?$/,
+        // Enable caching for improved performance during development
+        // It uses default OS directory by default. If you need something
+        // more custom, pass a path to it. I.e., babel?cacheDirectory=<path>
+        loaders: ['babel?cacheDirectory'],
+        // Parse only app files! Without this it will go through entire project.
+        // In addition to being slow, that will most likely result in an error.
+        include: PATHS.app
+      }
+    ]
+  },
+  sassLoader: {
+    includePaths: [path.resolve(__dirname, PATHS.style)]
+  }
+};
+
+
+// Default configuration
+if(TARGET === 'start' || !TARGET) {
+  module.exports = merge(common, {
+    devServer: {
+      contentBase: PATHS.build,
+
+      // Enable history API fallback so HTML5 History API based
+      // routing works. This is a good default that will come
+      // in handy in more complicated setups.
+      historyApiFallback: true,
+      hot: true,
+      inline: true,
+      progress: true,
+
+      // Display only errors to reduce the amount of output.
+      stats: 'errors-only',
+
+      // Parse host and port from env so this is easy to customize.
+      //
+      // If you use Vagrant or Cloud9, set
+      // host: process.env.HOST || '0.0.0.0';
+      //
+      // 0.0.0.0 is available to all network devices unlike default
+      // localhost
+      host: process.env.HOST,
+      port: process.env.PORT
+    },
+    plugins: [
+      new webpack.HotModuleReplacementPlugin(),
+      new NpmInstallPlugin({
+        save: true // --save
+      })
+    ]
+  });
+
+}
+
+if(TARGET === 'build') {
+  module.exports = merge(common, {});
+}
