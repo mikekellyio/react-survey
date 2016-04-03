e699a85e82fd5335f32f3a9e40e290f914d68a7f
Mike Kelly <mike@kellyfam.net>
2016-04-03 00:32:57 -0400

    remove extra menu items

--- app/components/Menu.jsx
+++ app/components/Menu.jsx
@@ -21,9 +21,6 @@ module.exports = React.createClass({
             </div>
             <div className="collapse navbar-collapse">
               <ul className="nav navbar-nav">
-                <li className="active"><a href="#">Home</a></li>
-                <li><a href="#about">About</a></li>
-                <li><a href="#contact">Contact</a></li>
               </ul>
             </div>
           </div>
